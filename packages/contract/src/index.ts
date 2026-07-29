1f8a7b3cf6dd8ecd9c4be95f9f0fab228757774e
sayweer <degirmenseyit@gmail.com>
2026-07-30 02:34:09 +0300

    feat: package deployable contract assets

--- packages/contract/src/index.ts
+++ packages/contract/src/index.ts
@@ -1,2 +1,14 @@
+import { CompiledContract } from '@midnight-ntwrk/midnight-js-protocol/compact-js';
+
 export * from './managed/aequira/contract/index.js';
 export * from './witnesses.js';
+
+import * as Aequira from './managed/aequira/contract/index.js';
+import * as Witnesses from './witnesses.js';
+
+export const compiledAequiraContract = CompiledContract.make<
+  Aequira.Contract<Witnesses.AequiraPrivateState>
+>('Aequira', Aequira.Contract<Witnesses.AequiraPrivateState>).pipe(
+  CompiledContract.withWitnesses(Witnesses.witnesses),
+  CompiledContract.withCompiledFileAssets('./managed/aequira'),
+);
